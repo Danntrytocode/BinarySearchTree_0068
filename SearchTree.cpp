fba31d7bad017baacdb73590bfb28fa45756b5cd
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-06-11 18:19:00 +0700

    add algorithm if pointer bukan NULL

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -82,7 +82,11 @@ class BinaryTree{
                 cout << "Tree is empty" << endl;
                 return;
             }
-            
+            if(ptr != NULL){
+                cout << ptr->info << " ";
+                preorder(ptr->leftchild);
+                preorder(ptr->rightchild);
+            }
         }
 
         void postorder(Node *ptr){}
